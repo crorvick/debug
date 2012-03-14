ed0492b37ced61d7356d7363d8da9bdaf7c6e43e
Chris Rorvick <chris.rorvick@gmail.com>
2012-03-14 01:02:11 -0500

    Added comments for usage.

--- debugstream.hpp
+++ debugstream.hpp
@@ -4,6 +4,40 @@
 
 #include <iosfwd>
 
+// A simple mechanism for writing debug info to an ostream.  Just
+// implement print_debug_details() for whatever type you are interested
+// in, optionally observing the verbose flag if distinct brief and
+// verbose outputs are desired.
+//
+// An example implementation for int's:
+//
+//   #include "debugstream.hpp"
+//
+//   #include <iostream>
+//
+//   void print_debug_details(std::ostream& o, const int& v, bool verbose)
+//   {
+//       if (verbose)
+//           o << "int @" << &v << " = " << v;
+//       else
+//           o << v;
+//   }
+//
+//   int main()
+//   {
+//       int i = 12;
+//
+//       std::cout
+//          << "test 1: " << debug::brief(i) << '\n'
+//          << "test 2: " << debug::verbose(i) << std::endl;
+//   }
+//
+// Compiling and running this example produces the following output:
+//
+//   $ ./example
+//   test 1: 12
+//   test 2: int @0xbfc1acfc = 12
+
 namespace debug {
 
     namespace detail {

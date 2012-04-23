591d4eac8bfa5d78e8b8b69191858fece966493c
Chris Rorvick <chris@rorvick.com>
2012-04-22 22:43:58 -0500

    Converted comments to Doxygen format.

--- debugstream.hpp
+++ debugstream.hpp
@@ -1,42 +1,54 @@
 
-#ifndef DEBUG_STREAM_H_included
-#define DEBUG_STREAM_H_included
+#ifndef DEBUG_STREAM_HPP_included
+#define DEBUG_STREAM_HPP_included
+
+//! \file
+//! A simple mechanism for writing debug info to an output stream.
+//!
+//! Use Standard Library output streams to output debug information
+//! without defining explicit <tt>operator<<()</tt> overloads.  Instead,
+//! wrap instances of \c T in calls to \c debug::brief() and
+//! \c debug::verbose() when writing to the output stream.  Then simply
+//! implement
+//! <tt>void print_debug_details(std::ostream&, T&, bool verbose)</tt>
+//! for the type \c T you are interested in.  The \c verbose flag
+//! can be observed to provide distinct brief and verbose output
+//! levels are desired.
+//!
+//! An example implementation for \c int values:
+//!
+//! \code
+//! #include "debugstream.hpp"
+//!
+//! #include <iostream>
+//!
+//! void print_debug_details(std::ostream& o, const int& v, bool verbose)
+//! {
+//!     if (verbose)
+//!         o << "int @" << &v << " = " << v;
+//!     else
+//!         o << v;
+//! }
+//!
+//! int main()
+//! {
+//!     int i = 12;
+//!
+//!     std::cout
+//!         << "test 1: " << debug::brief(i) << '\n'
+//!         << "test 2: " << debug::verbose(i) << std::endl;
+//! }
+//! \endcode
+//!
+//! Compiling and running this example produces the following output:
+//!
+//! \code
+//! $ ./example
+//! test 1: 12
+//! test 2: int @0xbfc1acfc = 12
+//! \endcode
 
 #include <iosfwd>
-
-// A simple mechanism for writing debug info to an ostream.  Just
-// implement print_debug_details() for whatever type you are interested
-// in, optionally observing the verbose flag if distinct brief and
-// verbose outputs are desired.
-//
-// An example implementation for int's:
-//
-//   #include "debugstream.hpp"
-//
-//   #include <iostream>
-//
-//   void print_debug_details(std::ostream& o, const int& v, bool verbose)
-//   {
-//       if (verbose)
-//           o << "int @" << &v << " = " << v;
-//       else
-//           o << v;
-//   }
-//
-//   int main()
-//   {
-//       int i = 12;
-//
-//       std::cout
-//          << "test 1: " << debug::brief(i) << '\n'
-//          << "test 2: " << debug::verbose(i) << std::endl;
-//   }
-//
-// Compiling and running this example produces the following output:
-//
-//   $ ./example
-//   test 1: 12
-//   test 2: int @0xbfc1acfc = 12
 
 namespace debug {
 
@@ -54,12 +66,16 @@ namespace debug {
 
     }
 
+    //! \brief Provide brief output on an ouput stream.
+    //! \param value The value to output for.
     template <typename T>
     detail::Streamer<T> brief(const T& value)
     {
         return detail::Streamer<T>(value, false);
     }
 
+    //! \brief Provide verbose output on an ouput stream.
+    //! \param value The value to output for.
     template <typename T>
     detail::Streamer<T> verbose(const T& value)
     {
@@ -75,5 +91,4 @@ std::ostream& operator<<(std::ostream& o, const debug::detail::Streamer<T>& s)
     return o;
 }
 
-#endif  // DEBUGSTREAM_H_included
-
+#endif  // DEBUGSTREAM_HPP_included
